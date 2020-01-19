751dd3a826dc2ee029a2fab8528959c69151b58b
GuiroHerrera <40594238+GuiroHerrera@users.noreply.github.com>
2020-01-19 13:39:45 -0500

    ndjs

--- Tercer_Parcial/Labs/Nodejs/03server.js
+++ Tercer_Parcial/Labs/Nodejs/03server.js
@@ -1,14 +1,17 @@
 var express = require('express')
 var app = express()
 var portarg = process.argv[2];
+var a=6;
+var b=8;
+var suma= a + b;
+var strn=''+suma;
 app.get('/', function(req,res){
-    res.send('Hello World!')
+    res.send(strn)
 })
 //npm init para generar el gestor paquetes json
 //npm install express --save   instalacion express
 var server = app.listen(portarg, function(){
     var host = server.address().address
     var port = server.address().port
-
     console.log('Example app listening it http://%s:%s',host,port)
 })
